6703cc87ba9fdd0bdd6fc2e9212c65d0c8c27efe
antti joutsenniemi <tinpurittna@gmail.com>
2024-07-08 19:44:11 +0300

    started on dbmodels

---
+++ server/dbModels/furnitureModel.ts
@@ -0,0 +1,130 @@
+import { MongoClient } from 'mongodb';
+
+export interface StyleJson {
+    nonValidImage: boolean,
+    explanation: string,
+    colorThemes: {
+        dark: number,
+        light: number,
+        colorful: number,
+        earthy: number,
+        blackAndWhite: number,
+        pastel: number,
+        neutrals: number,
+        jewelTones: number,
+        metallics: number,
+        oceanic: number
+    },
+    designStyles: {
+        industrial: number,
+        scandinavian: number,
+        minimalist: number,
+        modern: number,
+        farmhouse: number,
+        artDeco: number,
+        bohemian: number,
+        traditional: number,
+        rustic: number,
+        glam: number,
+        contemporary: number,
+        transitional: number
+    }
+}
+
+export interface ScrapingData {
+    picUrl: string;
+    title: string;
+    quantity: string;
+    price: string;
+    productUrl: string;
+    styleJson?: StyleJson
+}
+
+const chairModel = (furnitureCategory : string) => {
+    const url : string = process.env.MONGO_ATLAS_URI ?? "";
+    const client = new MongoClient(url);
+    const dbName = 'redecofinderData';
+    if(furnitureCategory === ''){
+
+    }
+    const collection = 'chairCollection';
+    const db = client.db(dbName);
+    const chairCollection = db.collection(collection);
+
+    client.connect().catch(error => {
+        console.error('Failed to connect to MongoDB:', error);
+    });
+
+    //find one by id and return it as json
+    const fetchOneWithStyles = async (title : string) => {
+        try {
+            const result = await chairCollection.findOne({title : title, styleJson: { $exists: true }});
+            return result;
+        } catch (error) {
+            console.error('Connection to productInfo document failed with status code 100: ', error);
+            throw error;
+        } 
+    }
+    
+    //find all data and return an array
+    const fetchData = async () => {
+        try {
+            const result = await chairCollection.find({
+                styleJson: { $exists: true },
+                deleted: false
+            }).toArray();
+            return result;
+        } catch (error) {
+            console.error('Connection to test db failed with status code 101');
+            throw error;
+        } 
+    }
+
+    //add one datacell to document
+    const addData = async (scrapingData : ScrapingData) => {
+        try {
+            const result = await chairCollection.insertOne(scrapingData);
+            return result;
+        } catch (error) {
+            console.error('Connection to test db failed with status code 102');
+            throw error;
+        } 
+    }
+
+    //check products deleted from webshop and update deleted boolean value
+    const checkDeletedAndUpdate = async (titleArray : string[]) => {
+        try {
+            const query = { title: { $nin: titleArray } };
+            const update = { $set: { deleted: true } };
+            const result = await chairCollection.updateMany(query, update);
+        } catch (error) {
+            console.error('Connection to test db failed with status code 102');
+            throw error;
+        } 
+    }
+
+    //update product deleted value back to false
+    const updateDeleted = async (title : string) => {
+        try {
+            const query = { title: title };
+            const update = { $set: { deleted: false } };
+            const result = await chairCollection.updateOne(query, update);
+        } catch (error) {
+            console.error('Connection to test db failed with status code 102');
+            throw error;
+        } 
+    }
+
+    // Define more functions as needed
+
+    return {
+        fetchOneWithStyles,
+        fetchData,
+        addData,
+        checkDeletedAndUpdate,
+        updateDeleted
+        // Add more functions to export here
+    };
+}
+
+export default chairModel;
