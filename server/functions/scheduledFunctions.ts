2138346c8fcf2cced05cc7b41e7b0a46474f9853
Antti Joutsenniemi <tinpurittna@gmail.com>
2024-07-14 17:59:04 +0300

    reorganized url data also in other ts file

--- server/functions/scheduledFunctions.ts
+++ server/functions/scheduledFunctions.ts
@@ -1,6 +1,7 @@
 import * as cron from 'node-cron';
 import { scrapeWebsite } from './webScraping';
 import { combineScrapingAndAi } from './addStyleDataToScraping';
+import clientPublic from './../styleJson/clientPublic.json';
 
 const scrapeCategory = async (url : string, category : string) => {
     try {
@@ -13,11 +14,11 @@ const scrapeCategory = async (url : string, category : string) => {
 
 export const scrapeAndMakeAiData = async () => {
     const scrapePromises = [
-        scrapeCategory('https://www.tavaratrading.com/kaytetyt/?category[]=2&category[]=11', 'chairs'),
-        scrapeCategory('https://www.tavaratrading.com/kaytetyt/?category[]=29', 'sofas'),
-        scrapeCategory('https://www.tavaratrading.com/kaytetyt/?category[]=8', 'storage'),
-        scrapeCategory('https://www.tavaratrading.com/kaytetyt/?category[]=5&category[]=173&category[]=54&category[]=190', 'tables'),
-        scrapeCategory('https://www.tavaratrading.com/kaytetyt/?category[]=139', 'conference')
+        scrapeCategory(`${clientPublic.webStoreUrl}/kaytetyt/?category[]=2&category[]=11`, 'chairs'),
+        scrapeCategory(`${clientPublic.webStoreUrl}/kaytetyt/?category[]=29`, 'sofas'),
+        scrapeCategory(`${clientPublic.webStoreUrl}/kaytetyt/?category[]=8`, 'storage'),
+        scrapeCategory(`${clientPublic.webStoreUrl}/kaytetyt/?category[]=5&category[]=173&category[]=54&category[]=190`, 'tables'),
+        scrapeCategory(`${clientPublic.webStoreUrl}/kaytetyt/?category[]=139`, 'conference')
     ];
 
     try {
