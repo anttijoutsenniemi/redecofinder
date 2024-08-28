2b03309be27c8ebc7a4bce3f7aae923e9256c260
antti joutsenniemi <tinpurittna@gmail.com>
2024-08-28 21:25:14 +0300

    visual update

--- server/functions/scheduledFunctions.ts
+++ server/functions/scheduledFunctions.ts
@@ -35,6 +35,30 @@ export const scrapeAndMakeAiData = async () => {
     }
 };
 
+//this is from new products
+export const scrapeAndMakeAiDataNew = async () => {
+    const scrapePromises = [
+        scrapeCategory(`${clientPublic.webStoreUrl}/toimistokalusteet/3/tyo-satula-ja-valvomotuolit/uudet-tyotuolit`, 'new_tyotuolit'), //työtuolit
+        scrapeCategory(`${clientPublic.webStoreUrl}/toimistokalusteet/198/korkeat-poydat-ja-tuolit/uudet-korkeat-tuolit`, 'new_korkeat_tuolit'), //korkeat tuolit
+        scrapeCategory(`${clientPublic.webStoreUrl}/toimistokalusteet/12/neuvottelupoydat-ja-tuolit/uudet-neuvottelu-ja-asiakastuolit`, 'new_neuvottelu-_ja_asiakastuolit'), //neuvottelu- ja asiakastuolit
+        scrapeCategory(`${clientPublic.webStoreUrl}/kaytetyt/?category[]=29`, 'sohvat_nojatuolit_ja_rahit'), //sohvat, nojatuolit ja rahit
+        scrapeCategory(`${clientPublic.webStoreUrl}/kaytetyt/?category[]=8`, 'sailytyskalusteet'), //säilytyskalusteet
+        scrapeCategory(`${clientPublic.webStoreUrl}/kaytetyt/?category[]=5`, 'tyopoydat'), //työpöydät
+        scrapeCategory(`${clientPublic.webStoreUrl}/kaytetyt/?category[]=195`, 'korkeat_poydat'), //korkeat pöydät
+        scrapeCategory(`${clientPublic.webStoreUrl}/kaytetyt/?category[]=173`, 'neuvottelupoydat'), //neuvottelupöydät
+        scrapeCategory(`${clientPublic.webStoreUrl}/kaytetyt/?category[]=54`, 'sahkopoydat'), //sähköpöydät
+        scrapeCategory(`${clientPublic.webStoreUrl}/kaytetyt/?category[]=190`, 'sohva-_ja_pikkupoydat'), //sohva- ja pikkupöydät
+        scrapeCategory(`${clientPublic.webStoreUrl}/kaytetyt/?category[]=139`, 'neuvotteluryhmat') //neuvotteluryhmät
+    ];
+
+    try {
+        await Promise.all(scrapePromises);
+        console.log('All categories have been scraped and processed.');
+    } catch (error) {
+        console.error('Error in scraping one or more categories:', error);
+    }
+};
+
 export const setupCronJobs = (): void => {
     //here first number is the starting minute and the other is hours, so this runs on the first minute of every 12 hours
     cron.schedule('0 */12 * * *', scrapeAndMakeAiData, {
