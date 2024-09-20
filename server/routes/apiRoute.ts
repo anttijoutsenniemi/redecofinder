48e305504683cfca8a0e37389df3065a3ae02e54
laguagu <49702497+laguagu@users.noreply.github.com>
2024-09-20 09:36:54 +0300

    Add Serper google search endpoints

--- server/routes/apiRoute.ts
+++ server/routes/apiRoute.ts
@@ -76,4 +76,25 @@ apiRoute.post("/sendFeedback", async (req : express.Request, res : express.Respo
     }
 });
 
+apiRoute.post("/serperImageSearch", async (req: express.Request, res: express.Response): Promise<void> => {
+    try {
+        const searchQuery = req.body.searchQuery;
+        const searchResults = await searchSerperImages(searchQuery);
+        res.status(200).json(searchResults);
+        console.log('searchResults', searchResults);
+    } catch (e: any) {
+        res.status(404).json({ "error": `error fetching: ${e}` });
+    }
+});
+
+apiRoute.post("/serperImageSearchFiltered", async (req: express.Request, res: express.Response): Promise<void> => {
+    try {
+        const searchQuery = req.body.searchQuery;
+        let searchResults = await searchSerperImagesFiltered(searchQuery);
+        res.status(200).json(searchResults);
+    } catch (e: any) {
+        res.status(404).json({ "error": `error fetching: ${e}` });
+    }
+});
+
 export default apiRoute;
